1165be8c4718ed0ea37ea1e8d2e09eeb4cc6bc8a
Hilary Omondi <hilaryomondi08@gmail.com>
2026-08-19 09:43:35 +0300

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        hash_s = {}
+        hash_t = {}
+
+        for char in s:
+            hash_s[char] = hash_s.get(char, 0) + 1
+
+        for char in t:
+            hash_t[char] = hash_t.get(char, 0) + 1
+
+        return hash_s == hash_t
